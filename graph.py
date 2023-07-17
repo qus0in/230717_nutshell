f1629e20a3f68ee31aeaf2013f4a1a802ba25911
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-07-17 10:42:01 +0900

    openai_api_key

---
+++ graph.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import openai
+
+def system(msg):
+    return {"role" : "system", "content": msg}
+def user(msg):
+    return {"role" : "user", "content": msg}
+
+@st.cache_data
+def get_mermaid(extracted_text):
+    openai.api_key = st.secrets["openai_api_key"] # API Key
+    messages = [
+        system("I'm going to give you text extracted from a PDF textbook used to teach Python."),
+        system("Based on what you've been handed, give us a bullet point nutshell."),
+        system("Write a tree-structured mermaid markdown of the cleaned up nutshell in order of importance and relationship."),
+        system("I want it to cover important concepts in Python programming and have more than two levels of depth."),
+        user(extracted_text),
+        system("The output should only receive markdown wrapped in ```, and the contents of mermaid should be written in Korean."),
+        system("Don't show me any results other than mermaid."),
+    ]
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    return completion['choices'][0]['message']['content']
