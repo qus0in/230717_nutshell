2c6a1d0182536da23e2deb869bf58b6999a12713
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-07-17 10:19:41 +0900

    추출한 텍스트

---
+++ file.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from io import StringIO
+
+def handle_pdf():
+    label = "✅ 파이썬 학습 PDF 파일을 올려주세요"
+    # PDF 문서 경로 지정
+    # pdf_path = 'example.pdf'
+    pdf_path = st.file_uploader(
+        label,
+        type="pdf")
+    # st.write(pdf_path)
+    # PDF에서 텍스트 추출
+    # extracted_text = extract_text_from_pdf(pdf_path)
+    extracted_text = extract_text_from_pdf(pdf_path)
+    with st.expander("📝 추출한 텍스트"):
+        st.write(extracted_text)
+
+def extract_text_from_pdf(pdf_path):
+    resource_manager = PDFResourceManager()
+    return_string = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(resource_manager, return_string, codec=codec, laparams=laparams)
+    interpreter = PDFPageInterpreter(resource_manager, device)
+    with open(pdf_path, 'rb') as pdf_file:
+        for page in PDFPage.get_pages(pdf_file, check_extractable=True):
+            interpreter.process_page(page)
+
+        text = return_string.getvalue()
+    device.close()
+    return_string.close()
+    return text
+
