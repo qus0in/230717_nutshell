668fdfbbd7078839de614d5c7cbe698b038a1de5
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-07-17 10:20:40 +0900

    file.handle_pdf()

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,8 @@
 import streamlit as st
-
-def handle_pdf():
-    label = "✅ 파이썬 학습 PDF 파일을 올려주세요"
-    pdf_path = st.file_uploader(
-        label,
-        type="pdf")
-    st.write(pdf_path)
+import file
 
 def main():
-    handle_pdf()
+    file.handle_pdf()
 
 if __name__ == "__main__":
     main()
